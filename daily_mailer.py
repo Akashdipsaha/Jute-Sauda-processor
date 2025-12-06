e29846f08574cfc0ccdb458cf90eb6da0b737291
Akashdip Saha <99889022+Akashdipsaha@users.noreply.github.com>
2025-12-06 13:34:39 +0530

    Update daily_mailer.py

--- daily_mailer.py
+++ daily_mailer.py
@@ -20,7 +20,11 @@ SENDER_PASS = os.environ.get("SENDER_PASS")
 # Add as many emails as you want, separated by commas
 RECIPIENT_LIST = [
     "akashdip.saha@jute-india.com",
-    "officialakashdip.333@gmail.com", 
+    "officialakashdip.333@gmail.com",
+    "payal.sinha@jute-india.com,
+    "rishavkajaria@gmail.com",
+    "raghav@jute-india.com",
+    "skajaria@jute-india.com",
 ] 
 
 def get_ist_time():
@@ -151,6 +155,7 @@ def send_daily_email():
 
 if __name__ == "__main__":
     send_daily_email()
+
 
 
 
